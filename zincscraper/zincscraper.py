727c4939a27ebf979527863dc7dcbc5ed5c0e2a9
Jason Dufresne <jason.alex.d@protonmail.com>
2021-06-29 02:41:06 -0400

    updated database functionality

--- zincscraper/zincscraper.py
+++ zincscraper/zincscraper.py
@@ -4,12 +4,14 @@ import pandas as pd
 import datetime
 import pyodbc
 
-server = 'jim-chem02.rgarrison.net\ZINC'
-database = 'ZINCProd'
-username = 'jason'
-password = 'Jason123!'
-cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
-cursor = cnxn.cursor()
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=jim-chem02\zinc;'
+                      'Database=ZINCProd;'
+                      'Trusted_Connection=yes;'
+                      'username=jason;'
+                      'password=Jason123!;')
+
+cursor = conn.cursor()
 
 while True:
     csv_file = input('CSV file path: ')
@@ -17,46 +19,62 @@ while True:
     f_name, f_ext = os.path.splitext(csv_file)
 
     if f_ext == '.csv':
-        csv_path = os.path.dirname(csv_file)
         break
     else:
         print('Please input the path of a CSV file.')
-        continue
 
 time_start = datetime.datetime.now()
 
 data = pd.read_csv(csv_file)
 
-id_list = []
-testarray = []
+# testarray = []
+
+# cursor.execute("TRUNCATE TABLE ZINCProd.dbo.Prodmain")
 
 for i in range(len(data)):
     zinc_id = data.loc[i][0].split('__')[0]
     binding_affinity = data.loc[i][1]
-    url = 'https://zinc15.docking.org/substances/' + zinc_id
 
+    url = 'https://zinc15.docking.org/substances/' + zinc_id
     r = requests.get(url)
+
     df_list = pd.read_html(r.text)  # this parses all the tables in webpages to a list
     first_table = df_list[0]
+
     logp = first_table.loc[:, 'logP'][0]
     mwt = first_table.loc[:, 'Mwt'][0]
 
     if 'pH range' in str(df_list):
-        print('3D')
         third_table = df_list[2]
         tpsa = third_table.loc[:, 'tPSA Å²'][0]
         rotbonds = third_table.loc[:, 'Rotatablebonds'][0]
     else:
-        print('2D')
         tpsa = 'null'
         rotbonds = 'null'
 
     count = cursor.execute("""
-    INSERT INTO ZINCProd (zinc_id, binding_affinity, logp, mwt, tpsa, rotbonds) 
+    INSERT INTO ZINCProd.dbo.Prodmain (zincid, bindaff, logp, mwt, tpsa, rotbonds) 
     VALUES (?,?,?,?,?,?)""",
-    zinc_id, binding_affinity, logp, mwt, tpsa, rotbonds).rowcount
-    cnxn.commit()
-    print('Rows inserted: ' + str(count))
+    str(zinc_id), str(binding_affinity), str(logp), str(mwt), str(tpsa), str(rotbonds)).rowcount
+    conn.commit()
+
+    delete_dup = cursor.execute("""
+    WITH cte AS (
+        SELECT
+            zincid,
+            ROW_NUMBER() OVER (
+                PARTITION BY
+                    zincid
+                ORDER BY
+                    zincid
+            ) row_num
+        FROM
+            ZINCProd.dbo.Prodmain
+    )
+    DELETE FROM cte
+    WHERE row_num > 1;
+    """)
+    delete_dup.commit()
 
     # testarray.append([zinc_id, binding_affinity, first_table.loc[:, 'logP'][0],  first_table.loc[:, 'Mwt'][0], tpsa, rotbonds])
 
